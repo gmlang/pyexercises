6a0d19060ee7589b141ed4251ada03edd5a265ff
Guangming Lang <gmlangatgithub@gmail.com>
2013-10-08 23:23:35 -0400

    regex1-2
    3

--- regex/regex1.py
+++ regex/regex1.py
@@ -17,21 +17,21 @@ print pat.sub('{\g<0>}', s3)
 # remove repeating vowels
 pat = r'([aeiou])\1' # matches any vowel which is followed by itself again
 pat = re.compile(pat)
-pat.findall(s3)
+print pat.findall(s3)
 matches = pat.finditer(s3)
 for match in matches:
-    match.group(0)
-pat.sub(r'\1', s3) # \1 backreferences the vowel
+    print match.group(0)
+print pat.sub(r'\1', s3) # \1 backreferences the vowel
 
 # what if a vowel is repeated more than once?
 s4 = 'caaaaaabaaaceeeoooo'
-pat.sub(r'\1', s4)
+print pat.sub(r'\1', s4)
 
 pat = r'([aeiou])\1+' # matches any vowel which is followed by itself one or more times
 pat = re.compile(pat)
-pat.findall(s4)
+print pat.findall(s4)
 matches = pat.finditer(s4)
 for match in matches:
-    match.group(0)
-pat.sub(r'\1', s4) # \1 backreferences the vowel
+    print match.group(0)
+print pat.sub(r'\1', s4) # \1 backreferences the vowel
  
--- regex/regex2.py
+++ regex/regex2.py
@@ -12,69 +12,69 @@ For the times they are a-changin'''
 # .group(2) => the 2nd matched item specified by the 2nd pair of parentheses in the pattern
 
 pattern = r'(And)( )(the)'
-re.compile(pattern).match(s).groups() # ('And', ' ', 'the')
-re.compile(pattern).match(s).group(0) # 'And the'
-re.compile(pattern).match(s).group(1) # 'And'
-re.compile(pattern).match(s).group(2) # ' ' 
-re.compile(pattern).match(s).group(3) # 'the'
-re.compile(pattern).sub(r'\1', s) # 'And first one now will later be last\nFor the times they are a-changin'
-re.compile(pattern).sub(r'\2', s) # ' first one now will later be last\nFor the times they are a-changin'
-re.compile(pattern).sub(r'\3', s) # 'the first one now will later be last\nFor the times they are a-changin'
+print re.compile(pattern).match(s).groups() # ('And', ' ', 'the')
+print re.compile(pattern).match(s).group(0) # 'And the'
+print re.compile(pattern).match(s).group(1) # 'And'
+print re.compile(pattern).match(s).group(2) # ' ' 
+print re.compile(pattern).match(s).group(3) # 'the'
+print re.compile(pattern).sub(r'\1', s) # 'And first one now will later be last\nFor the times they are a-changin'
+print re.compile(pattern).sub(r'\2', s) # ' first one now will later be last\nFor the times they are a-changin'
+print re.compile(pattern).sub(r'\3', s) # 'the first one now will later be last\nFor the times they are a-changin'
 
 s3 = 'caabaaceeoo'
 
 # .search matches anywhere within the string, only return one matchObject
 pat = r'(a)(b)(a)'
-re.search(pat, s1).groups() # ('a', 'b', 'a')
-re.search(pat, s1).group(0) # 'aba'
-re.search(pat, s1).group(1) # 'a'
-re.search(pat, s1).group(2) # 'b'
-re.search(pat, s1).group(3) # 'a'
+print re.search(pat, s1).groups() # ('a', 'b', 'a')
+print re.search(pat, s1).group(0) # 'aba'
+print re.search(pat, s1).group(1) # 'a'
+print re.search(pat, s1).group(2) # 'b'
+print re.search(pat, s1).group(3) # 'a'
 
 pat = r'(a)'
-re.search(pat, s1).groups() # ('a',)
-re.search(pat, s1).group(0) # 'a'
-re.search(pat, s1).group(1) # 'a'
-re.search(pat, s1).group(2) # IndexError: no such group
+print re.search(pat, s1).groups() # ('a',)
+print re.search(pat, s1).group(0) # 'a'
+print re.search(pat, s1).group(1) # 'a'
+print re.search(pat, s1).group(2) # IndexError: no such group
 
 pat = r'(aa)'
-re.search(pat, s1).groups() # ('aa',)
-re.search(pat, s1).group(0) # 'aa'
-re.search(pat, s1).group(1) # 'aa'
-re.search(pat, s1).group(2) # IndexError: no such group
+print re.search(pat, s1).groups() # ('aa',)
+print re.search(pat, s1).group(0) # 'aa'
+print re.search(pat, s1).group(1) # 'aa'
+print re.search(pat, s1).group(2) # IndexError: no such group
 
 pat = r'(a)\1' # this pattern is different from r'(aa)'
-re.search(pat, s3).groups() # ('a',)
-re.search(pat, s3).group(0) # 'aa'
-re.search(pat, s3).group(1) # 'a'
-re.search(pat, s3).group(2) # IndexError: no such group
+print re.search(pat, s3).groups() # ('a',)
+print re.search(pat, s3).group(0) # 'aa'
+print re.search(pat, s3).group(1) # 'a'
+print re.search(pat, s3).group(2) # IndexError: no such group
 
 pat = r'([aeiou])\1' # matches any vowel which is followed by itself again
 pat = re.compile(pat)
 matches = pat.finditer(s3)
 for match in matches:
-    match.group(0)
+    print match.group(0)
 # 'aa'
 # 'aa'
 # 'ee'
 # 'oo'    
-pat.sub(r'\1', s3)
+print pat.sub(r'\1', s3)
 
 s4 = 'caaaaaabaaaceeeoooo'
 matches = pat.finditer(s4)
 for match in matches:
-    match.group(0)
+    print match.group(0)
 
 pat = r'([aeiou])\1+' # matches any vowel which is followed by itself one or more times
 pat = re.compile(pat)
-pat.findall(s4)
+print pat.findall(s4)
 matches = pat.finditer(s4)
 for match in matches:
-    match.group(0)
+    print match.group(0)
 # 'aaaaaa'
 # 'aaa'
 # 'eee'
 # 'oooo'    
     
-pat.sub(r'\1', s4) # \1 backreferences the vowel
+print pat.sub(r'\1', s4) # \1 backreferences the vowel
  
